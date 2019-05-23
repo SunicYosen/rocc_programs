9fde9db7f552a25f8e16194ac7feb1ffcf3ce922
sunicyosen <sunicyosen@outlook.com>
2019-05-23 16:02:39 +0800

    modified isa program with rocc-tests

--- src/isa/rocc_cnn.h
+++ src/isa/rocc_cnn.h
@@ -6,7 +6,7 @@
 #ifndef ROCC_CNN_H
 #define ROCC_CNN_H
 
-#include "xcustom.h"
+#include "src/xcustom.h"
 
 #define XCUSTOM_ACC 0
 #define SEND_DATA 1
@@ -14,5 +14,20 @@
 #define GET_FLAG 3
 #define IS_ISSUE_FLAG 0
 
+#define CUSTOM_X 0
+#define a0_reg 10
+#define a1_reg 11
+#define a2_reg 12
+#define a3_reg 13
+
+
+#define GET_FLAG_FUNC(flag_reg, flag_type) \
+        ROCC_INSTRUCTION_RAW_R_R_R(XCUSTOM_ACC, flag_reg, flag_type, 0, GET_FLAG)
+
+#define SEND_DATA_FUNC(return_state, data_reg, rocc_reg) \
+        ROCC_INSTRUCTION_RAW_R_R_R(XCUSTOM_ACC, return_state, data_reg, rocc_reg, SEND_DATA)
+
+#define SEND_ISA_FUNC(return_state, command_reg) \
+        ROCC_INSTRUCTION_RAW_R_R_R(XCUSTOM_ACC, return_state, command_reg, 0, SEND_ISA)
 
 #endif
